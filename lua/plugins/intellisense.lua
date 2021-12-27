56f7cbddadac4e9d7d96d1b06cfd61f04ebcaea1
cpuccino <thegreatcappuccino@gmail.com>
2021-12-27 22:30:49 +1100

    feat: Updated completion formatting

--- lua/plugins/intellisense.lua
+++ lua/plugins/intellisense.lua
@@ -12,6 +12,40 @@ local feedkey = function(key, mode)
   api.nvim_feedkeys(api.nvim_replace_termcodes(key, true, true, true), mode, true)
 end
 
+local kind_icons = {
+  Text = '',
+  Method = 'm',
+  Function = '',
+  Constructor = '',
+  Field = '',
+  Variable = '',
+  Class = '',
+  Interface = '',
+  Module = '',
+  Property = '',
+  Unit = '',
+  Value = '',
+  Enum = '',
+  Keyword = '',
+  Snippet = '',
+  Color = '',
+  File = '',
+  Reference = '',
+  Folder = '',
+  EnumMember = '',
+  Constant = '',
+  Struct = '',
+  Event = '',
+  Operator = '',
+  TypeParameter = ''
+};
+
+local menu_label = {
+  vsnip = '[Snippet]',
+  buffer = '[Buffer]',
+  path = '[Path]'
+};
+
 function mod.configure()
   local cmp_ok, cmp = pcall(require, 'cmp');
   if not cmp_ok then
@@ -56,11 +90,18 @@ function mod.configure()
         end
       end, { 'i', 's' })
     },
+    formatting = {
+      fields = { 'kind', 'abbr', 'menu' },
+      format = function(entry, item)
+        item.kind = string.format('%s', kind_icons[item.kind]);
+        item.menu = menu_label[entry.source.name];
+        return item;
+      end
+    },
     sources = cmp.config.sources({
+      { name = 'vsnip' },
       { name = 'buffer' },
-      { name = "path" },
-      { name = 'cmdline' },
-      { name = 'vsnip' }
+      { name = 'path' }
     }),
   };
 
